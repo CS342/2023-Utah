33c14cdbf3eab6ce1769cfcc2eab68194ba169dd
emmy <emmythama@gmail.com>
2023-02-15 16:52:33 -0800

    Changed tests

--- UtahUITests/OnboardingTests.swift
+++ UtahUITests/OnboardingTests.swift
@@ -25,6 +25,23 @@ class OnboardingTests: XCTestCase {
     func testOnboardingFlow() throws {
         let app = XCUIApplication()
         
+//        let nextButton = XCUIApplication().scrollViews.otherElements.buttons["Next"]
+//        nextButton.tap()
+//        nextButton.tap()
+//        
+//        let elementsQuery = XCUIApplication().scrollViews.otherElements
+//        let nextButton = elementsQuery.buttons["Next"]
+//        nextButton.tap()
+//        nextButton.tap()
+//        elementsQuery.textFields["Enter your given name ..."].tap()
+//        
+//        let enterYourFamilyNameTextField = elementsQuery.textFields["Enter your family name ..."]
+//        enterYourFamilyNameTextField.tap()
+//        enterYourFamilyNameTextField.tap()
+//        elementsQuery.children(matching: .scrollView).element.swipeRight()
+//        elementsQuery.buttons["I Consent"].tap()
+//        elementsQuery.buttons["Grant Access"].tap()
+//                        
         try app.navigateOnboardingFlow(assertThatHealthKitConsentIsShown: true)
         
         let tabBar = app.tabBars["Tab Bar"]
@@ -39,7 +56,7 @@ class OnboardingTests: XCTestCase {
 
 extension XCUIApplication {
     func conductOnboardingIfNeeded() throws {
-        if self.staticTexts["CardinalKit\nUtah Application"].waitForExistence(timeout: 0.5) {
+        if self.staticTexts["Welcome to\nVascuTrack"].waitForExistence(timeout: 0.5) {
             try navigateOnboardingFlow(assertThatHealthKitConsentIsShown: false)
         }
     }
@@ -47,92 +64,92 @@ extension XCUIApplication {
     func navigateOnboardingFlow(assertThatHealthKitConsentIsShown: Bool = true) throws {
         try navigateOnboardingFlowWelcome()
         try navigateOnboardingFlowInterestingModules()
-        if staticTexts["Consent Example"].waitForExistence(timeout: 0.5) {
+        if staticTexts["Consent"].waitForExistence(timeout: 0.5) {
             try navigateOnboardingFlowConsent()
         }
         try navigateOnboardingFlowHealthKitAccess(assertThatHealthKitConsentIsShown: assertThatHealthKitConsentIsShown)
     }
     
     private func navigateOnboardingFlowWelcome() throws {
-        XCTAssertTrue(staticTexts["CardinalKit\nUtah Application"].waitForExistence(timeout: 0.5))
-        XCTAssertTrue(
-            staticTexts["This application demonstrates several CardinalKit features & modules."]
-                .waitForExistence(timeout: 0.5)
-        )
-        
-        XCTAssertTrue(staticTexts["The CardinalKit Framework"].waitForExistence(timeout: 0.5))
-        XCTAssertTrue(
-            staticTexts["The CardinalKit Framework builds the foundation of this template application."]
-                .waitForExistence(timeout: 0.5)
-        )
-        
-        XCTAssertTrue(staticTexts["Swift Package Manager"].waitForExistence(timeout: 0.5))
-        XCTAssertTrue(
-            staticTexts["CardinalKit uses the Swift Package Manager to import it as a dependency."]
-                .waitForExistence(timeout: 0.5)
-        )
-        
-        XCTAssertTrue(staticTexts["CardinalKit Modules"].waitForExistence(timeout: 0.5))
-        XCTAssertTrue(
-            staticTexts["CardinalKit offers several modules including HealthKit integration, questionnaires, and more ..."]
-                .waitForExistence(timeout: 0.5)
-        )
-        
-        XCTAssertTrue(buttons["Learn More"].waitForExistence(timeout: 0.5))
-        buttons["Learn More"].tap()
+//        XCTAssertTrue(staticTexts["Welcome to\nVascuTrack"].waitForExistence(timeout: 1.0))
+//        XCTAssertTrue(
+//            staticTexts["This application demonstrates several CardinalKit features & modules."]
+//                .waitForExistence(timeout: 0.5)
+//        )
+//
+//        XCTAssertTrue(staticTexts["Welcome to\nVascuTrack"].waitForExistence(timeout: 0.5))
+//        XCTAssertTrue(
+//            staticTexts["The CardinalKit Framework builds the foundation of this template application."]
+//                .waitForExistence(timeout: 0.5)
+//        )
+//
+//        XCTAssertTrue(staticTexts["Swift Package Manager"].waitForExistence(timeout: 0.5))
+//        XCTAssertTrue(
+//            staticTexts["CardinalKit uses the Swift Package Manager to import it as a dependency."]
+//                .waitForExistence(timeout: 0.5)
+//        )
+//
+//        XCTAssertTrue(staticTexts["CardinalKit Modules"].waitForExistence(timeout: 0.5))
+//        XCTAssertTrue(
+//            staticTexts["CardinalKit offers several modules including HealthKit integration, questionnaires, and more ..."]
+//                .waitForExistence(timeout: 0.5)
+//        )
+//
+        XCTAssertTrue(buttons["Next"].waitForExistence(timeout: 0.5))
+        buttons["Next"].tap()
     }
     
     private func navigateOnboardingFlowInterestingModules() throws {
-        XCTAssertTrue(staticTexts["Interesting Modules"].waitForExistence(timeout: 0.5))
-        XCTAssertTrue(
-            staticTexts["Here are a few CardinalKit modules that are interesting to know about ..."]
-                .waitForExistence(timeout: 0.5)
-        )
-        
-        XCTAssertTrue(staticTexts["Onboarding"].waitForExistence(timeout: 0.5))
-        XCTAssertTrue(
-            staticTexts["The onboarding module allows you to build an onboarding flow like this one."]
-                .waitForExistence(timeout: 0.5)
-        )
+        XCTAssertTrue(staticTexts["What condition do you have?"].waitForExistence(timeout: 0.5))
+//        XCTAssertTrue(
+//            staticTexts["Here are a few CardinalKit modules that are interesting to know about ..."]
+//                .waitForExistence(timeout: 0.5)
+//        )
+//
+//        XCTAssertTrue(staticTexts["Onboarding"].waitForExistence(timeout: 0.5))
+//        XCTAssertTrue(
+//            staticTexts["The onboarding module allows you to build an onboarding flow like this one."]
+//                .waitForExistence(timeout: 0.5)
+//        )
+//        XCTAssertTrue(buttons["Next"].waitForExistence(timeout: 0.5))
+//        buttons["Next"].tap()
+//
+//        XCTAssertTrue(staticTexts["FHIR"].waitForExistence(timeout: 0.5))
+//        XCTAssertTrue(
+//            staticTexts["The FHIR module provides a CardinalKit standard that can be used as a communication standard between modules."]
+//                .waitForExistence(timeout: 0.5)
+//        )
         XCTAssertTrue(buttons["Next"].waitForExistence(timeout: 0.5))
         buttons["Next"].tap()
         
-        XCTAssertTrue(staticTexts["FHIR"].waitForExistence(timeout: 0.5))
-        XCTAssertTrue(
-            staticTexts["The FHIR module provides a CardinalKit standard that can be used as a communication standard between modules."]
-                .waitForExistence(timeout: 0.5)
-        )
-        XCTAssertTrue(buttons["Next"].waitForExistence(timeout: 0.5))
-        buttons["Next"].tap()
-        
-        XCTAssertTrue(staticTexts["Contact"].waitForExistence(timeout: 0.5))
-        XCTAssertTrue(
-            staticTexts["The contact module allows you to display contact information in your application."]
-                .waitForExistence(timeout: 0.5)
-        )
-        XCTAssertTrue(buttons["Next"].waitForExistence(timeout: 0.5))
-        buttons["Next"].tap()
-        
-        XCTAssertTrue(staticTexts["HealthKit Data Source"].waitForExistence(timeout: 0.5))
-        XCTAssertTrue(
-            staticTexts["The HealthKit data source module allows you to fetch data from HealthKit and e.g. transform it to FHIR resources."]
-                .waitForExistence(timeout: 0.5)
-        )
-        
-        XCTAssertTrue(buttons["Next"].waitForExistence(timeout: 0.5))
-        buttons["Next"].tap()
+//        XCTAssertTrue(staticTexts["Contact"].waitForExistence(timeout: 0.5))
+//        XCTAssertTrue(
+//            staticTexts["The contact module allows you to display contact information in your application."]
+//                .waitForExistence(timeout: 0.5)
+//        )
+//        XCTAssertTrue(buttons["Next"].waitForExistence(timeout: 0.5))
+//        buttons["Next"].tap()
+//
+//        XCTAssertTrue(staticTexts["HealthKit Data Source"].waitForExistence(timeout: 0.5))
+//        XCTAssertTrue(
+//            staticTexts["The HealthKit data source module allows you to fetch data from HealthKit and e.g. transform it to FHIR resources."]
+//                .waitForExistence(timeout: 0.5)
+//        )
+//
+//        XCTAssertTrue(buttons["Next"].waitForExistence(timeout: 0.5))
+//        buttons["Next"].tap()
     }
     
     private func navigateOnboardingFlowConsent() throws {
-        XCTAssertTrue(staticTexts["Consent Example"].waitForExistence(timeout: 0.5))
-        XCTAssertTrue(
-            staticTexts["CardinalKit can collect consent from a user. You can provide the consent document using a markdown file."]
-                .waitForExistence(timeout: 0.5)
-        )
-        XCTAssertTrue(
-            staticTexts["CardinalKit can render consent documents in the markdown format: This is a markdown example.\n"]
-                .waitForExistence(timeout: 0.5)
-        )
+        XCTAssertTrue(staticTexts["Consent"].waitForExistence(timeout: 0.5))
+//        XCTAssertTrue(
+//            staticTexts["CardinalKit can collect consent from a user. You can provide the consent document using a markdown file."]
+//                .waitForExistence(timeout: 0.5)
+//        )
+//        XCTAssertTrue(
+//            staticTexts["CardinalKit can render consent documents in the markdown format: This is a markdown example.\n"]
+//                .waitForExistence(timeout: 0.5)
+//        )
         
         XCTAssertTrue(staticTexts["Given Name"].waitForExistence(timeout: 0.5))
         staticTexts["Given Name"].tap()
@@ -152,10 +169,10 @@ extension XCUIApplication {
     }
     
     private func navigateOnboardingFlowHealthKitAccess(assertThatHealthKitConsentIsShown: Bool = true) throws {
-        XCTAssertTrue(staticTexts["HealthKit Access"].waitForExistence(timeout: 0.5))
-        XCTAssertTrue(
-            staticTexts["CardinalKit can access data from HealthKit using the HealthKitDataSource module."].waitForExistence(timeout: 0.5)
-        )
+//        XCTAssertTrue(staticTexts["HealthKit"].waitForExistence(timeout: 0.5))
+//        XCTAssertTrue(
+//            staticTexts["CardinalKit can access data from HealthKit using the HealthKitDataSource module."].waitForExistence(timeout: 0.5)
+//        )
         XCTAssertTrue(images["heart.text.square.fill"].waitForExistence(timeout: 0.5))
         XCTAssertTrue(buttons["Grant Access"].waitForExistence(timeout: 0.5))
         
