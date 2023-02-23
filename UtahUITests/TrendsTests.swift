9702ec68e4e103ef871f154c403c22166759d076
Jennifer Xu <jennxu23@stanford.edu>
2023-02-23 13:54:38 -0800

    trends test

--- UtahUITests/TrendsTests.swift
+++ UtahUITests/TrendsTests.swift
@@ -13,21 +13,35 @@ class TrendsTests: XCTestCase {
     override func setUpWithError() throws {
         try super.setUpWithError()
         
+        try disablePasswordAutofill()
+        
         continueAfterFailure = false
         
         let app = XCUIApplication()
-        app.launchArguments = ["--skipOnboarding"]
-        app.launch()
+        app.launchArguments = ["--showOnboarding"]
+        app.deleteAndLaunch(withSpringboardAppName: "Utah")
     }
     
     
     func testTrends() throws {
         let app = XCUIApplication()
+        try app.conductOnboardingIfNeeded()
+        try exitAppAndOpenHealth(.steps)
+        app.activate()
+        sleep(5)
+        try navigateToTrends()
         
+        
+        
+    }
+    
+    func navigateToTrends() throws {
+        let app = XCUIApplication()
         XCTAssertTrue(app.tabBars["Tab Bar"].buttons["Trends"].waitForExistence(timeout: 0.5))
         app.tabBars["Tab Bar"].buttons["Trends"].tap()
         
         XCTAssertTrue(app.staticTexts["Daily Step Count"].waitForExistence(timeout: 0.5))
+        XCTAssertTrue(app.staticTexts["42.0"].waitForExistence(timeout: 0.5))
         XCTAssertTrue(app.staticTexts["Survey Score"].waitForExistence(timeout: 0.5))
     }
 }
