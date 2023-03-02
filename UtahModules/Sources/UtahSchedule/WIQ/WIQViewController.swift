ea0c889af60dd93a230780ebad19e34ec906b65e
Brooke Krajancich <brookekrajancich@gmail.com>
2023-03-01 21:45:34 -0800

    trying again

--- UtahModules/Sources/UtahSchedule/WIQ/WIQViewController.swift
+++ UtahModules/Sources/UtahSchedule/WIQ/WIQViewController.swift
@@ -7,7 +7,7 @@
 //
 
 // swiftlint:disable line_length
-// swiftlint:disable closure_body_length superfluous_disable_command
+// swiftlint:disable closure_body_length
 
 import ResearchKit
 import SwiftUI
